04d44a18519f6c9dd67bb558a9459df140ea4d66
xia1997x <48220443+harukodi@users.noreply.github.com>
2024-09-27 09:56:46 +0200

    Update logger.py

--- logger.py
+++ logger.py
@@ -4,19 +4,22 @@ class logger:
         self.logger = True
         self.current_time = time.strftime("%Y_%m_%d %H_%M_%S", time.localtime())
         self.path = f"./log-{self.current_time}"
+        if self.logger != True:
+            print("logger has been disabled")
+    
     def append_log(self, log_content):
         try:
             if self.logger == True:
                 with open(self.path, "x") as file:
                     file.write(log_content + "\n")
             else:
-                print("logger is disabled")
+                pass
         except FileExistsError:
             if self.logger == True:
                 with open(self.path, "a") as file:
                     file.write(log_content + "\n")
             else:
-                print("logger is disabled")
+                pass
 
 
 logger_instance = logger()
