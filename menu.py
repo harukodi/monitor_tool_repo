7e053f7b4905716fc8643c50f9f61fd264843c2b
xia1997x <48220443+harukodi@users.noreply.github.com>
2024-10-08 14:59:14 +0200

    menu fixes and some refactoring of the code

--- menu.py
+++ menu.py
@@ -10,23 +10,56 @@ import time, os, sys, platform, keyboard, msvcrt
 logger_class = logger()
 
 def monitor_render(refresh_interval):
-    def move_console_cursor_up(lines):
-        sys.stdout.write(f"\033[{lines}A")
-        sys.stdout.flush()
-    
-    time.sleep(refresh_interval)
-    print(f"CPU: {cpu.get_cpu_usage()}%")
-    print(f"RAM: Currently using {ram.get_ram_stats()['used']} GB of RAM out of {ram.get_ram_stats()['total']} GB available.")
-    print(f"Currently using {disk.get_disk_stats()['used']} GB of disk space out of {disk.get_disk_stats()['total']} GB available, with {disk.get_disk_stats()['free']} GB free.")
-    move_console_cursor_up(100)
+    try:
+        def move_console_cursor_up(lines):
+            sys.stdout.write(f"\033[{lines}A")
+            sys.stdout.flush()
+        while True:
+            time.sleep(refresh_interval)
+            print(f"CPU: {cpu.get_cpu_usage()}%")
+            print(f"RAM: Currently using {ram.get_ram_stats()['used']} GB of RAM out of {ram.get_ram_stats()['total']} GB available.")
+            print(f"Currently using {disk.get_disk_stats()['used']} GB of disk space out of {disk.get_disk_stats()['total']} GB available, with {disk.get_disk_stats()['free']} GB free.")
+            move_console_cursor_up(100)
+    except KeyboardInterrupt:
+        pass
 
 def clear_console():
     os.system("cls")
 
+def show_configured_alarms():
+    try:
+        if len(cpu_alarms) != 0 or len(cpu_alarms) != 0:
+            print("Alarms")
+            print("-" * 40)
+            if len(cpu_alarms) != 0:
+                for cpu_alarm in cpu_alarms:
+                    # Showing alarms for cpu where cpu_alarm[1] gets the name of the alarm and cpu_alarm[0] gets the threshold
+                    print(f"{cpu_alarm[1]}: {cpu_alarm[0]}%")
+            if len(ram_alarms) != 0:
+                for ram_alarm in ram_alarms:
+                    # Showing alarms for ram where ram_alarm[1] gets the name of the alarm and ram_alarm[0] gets the threshold
+                    print(f"{ram_alarm[1]}: {ram_alarm[0]}%")
+            print("-" * 40)
+            print("Press CTRL + C to go back")
+            while True:
+                try:
+                  continue
+                except KeyboardInterrupt:
+                  pass
+    except KeyboardInterrupt:
+        pass
+            
+    if len(cpu_alarms) == 0 and len(ram_alarms) == 0:
+        print("No alarms have been configured\nReturning to the main menu...")
+        time.sleep(2.2)
+
 def alarm_selection():
+    print("-" * 40)
     print("1: Add cpu alarm")
     print("2: Add ram alarm")
     print("3: Add disk alarm")
+    print("-" * 40)
+    
     input_selection_win = msvcrt.getch().decode('utf-8')
     if input_selection_win == "1":
         clear_console()
@@ -40,60 +73,41 @@ def alarm_selection():
         ram_alarm_name = input("RAM alarm name: ")
         append_ram_alarm(ram_threshold_input, ram_alarm_name)
         logger_class.append_log("ram_alarm_added")
+    elif input_selection_win == input_selection_win:
+        print("Not an option\nReturning to the main menu...")
+        time.sleep(2.2)
         
         
 def menu_selections():
     global user_input
     clear_console()
-    print("1: Start monitoring")
-    print("2: Add Alarms")
-    print("3: Show configured alarms")
-    print("5: Exit")
-    print("Press enter to clear console")
+    print("-" * 40)
+    print("1. Start Monitoring")
+    print("2. Add Alarms")
+    print("3. Show Configured Alarms")
+    print("5. Exit")
+    print("-" * 40)
+    print("\nPress ENTER to clear console.")
+
     input_selection_win = msvcrt.getch().decode('utf-8')
-    
     if input_selection_win == "1":
         clear_console()
         logger_class.append_log("MONITORING_STARTED")
-        while True:
-            monitor_render(refresh_interval_per_sec)
+        monitor_render(refresh_interval_per_sec)
             
     elif input_selection_win == "2":
         clear_console()
         alarm_selection()
-        
+    
     elif input_selection_win == "3":
         clear_console()
-        if len(cpu_alarms) != 0:
-            for cpu_alarm in cpu_alarms:
-                # Showing alarms for cpu where cpu_alarm[1] gets the name of the alarm and cpu_alarm[0] gets the threshold
-                print(f"{cpu_alarm[1]}: {cpu_alarm[0]}%")
-        if len(ram_alarms) != 0:
-            for ram_alarm in ram_alarms:
-                # Showing alarms for ram where ram_alarm[1] gets the name of the alarm and ram_alarm[0] gets the threshold
-                print(f"{ram_alarm[1]}: {ram_alarm[0]}%")
-        if len(cpu_alarms) == 0 and len(ram_alarms) == 0:
-            print("No alarms have been configured")
-        print("press B and then enter to go back")
-        while True:
-            if keyboard.read_key() == "b":
-                keyboard.wait("enter")
-                clear_console()
-                break
-    
+        show_configured_alarms()
+
     elif input_selection_win == "5":
         clear_console()
         logger_class.append_log("EXITED")
         exit()
 
 def start_menu():
-    time.sleep(1)
     while True:
-        try:
-            menu_selections()
-        except ValueError:
-            pass
-        except KeyboardInterrupt:
-            clear_console()
-            logger_class.append_log("EXITED")
-            exit()
+        menu_selections()
